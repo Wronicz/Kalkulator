d9d03c2b7cf6cd0f703b34e878ebb6d3c63d529e
Wronicz <124258071+Wronicz@users.noreply.github.com>
2023-02-03 09:14:34 +0100

    2 files modified

---
+++ index.js
@@ -0,0 +1,19 @@
+// How many seconds are between
+// 2 July 2014 12:30:07.999 and 2 July 2014 12:30:20.000?
+//const  = zmienna (
+  //new Date(2014, 6, 2, 12, 30, 20, 0),
+  //new Date(2014, 6, 2, 12, 30, 7, 999)
+//)
+//=> 12
+
+
+
+// Calculate milliseconds in a year
+//const minute = 1000 * 60;
+//const hour = minute * 60;
+//const day = hour * 24;
+//const year = day * 365;
+
+// Divide Time with a year
+//const d = new Date();
+//let seconds = Math.round(d.getTime() / year);
